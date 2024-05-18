610cd1e0540e1d0171f9d0cdc5cec811e3bf995d
DuaAmjad <133864832+DuaAmjad@users.noreply.github.com>
2024-05-18 18:09:14 +0500

    Add files via upload

---
+++ masksketch/configs/base_config.py
@@ -0,0 +1,54 @@
+
+
+"""Base configuration and hyperparameters."""
+
+import ml_collections
+
+def get_config():
+  """Get the default hyperparameter configuration."""
+  config = ml_collections.ConfigDict()
+  config.seed = 42
+  config.image_size = 256
+  config.batch_size = 256
+  config.eval_batch_size = 64
+  config.dtype = "float32"
+  config.dataset = "imagenet2012"
+  config.save_train_iter = False
+
+  config.experiment = ""
+  config.polyak_decay = 0.999
+  config.shuffle_buffer_size = 1000
+  config.train_shuffle = True
+  config.eval_pad_last_batch = False
+  config.batch_norm_group_size = -1
+  config.pretrained_image_model = False
+
+  config.eval_num = 30000
+  config.eval_avg_num = 1
+  config.eval_exact_match = True
+  config.eval_is_splits = 1
+
+  config.num_train_steps = 1_000_000
+  config.eval_every_steps = 100
+  config.eval_random_show = True
+  config.eval_show_num = 16
+  config.checkpoint_every_steps = 10_000
+
+  config.optimizer = ml_collections.ConfigDict()
+  config.optimizer.type = "adam"
+  config.optimizer.lr = 0.0001
+  config.optimizer.beta1 = 0.0
+  config.optimizer.beta2 = 0.999
+
+  config.optimizer.lr_schedule = "constant"
+  config.optimizer.lr_max = 1e-4
+  config.optimizer.lr_min = 0.0
+  config.optimizer.lr_warmup_steps = 2_000
+  config.optimizer.lr_decay_steps = 98_000
+  return config
+
+
+def get_hyper(h):
+  return h.product([
+      h.sweep("trial", range(1)),
+  ], name="config")
